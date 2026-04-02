634863e14500c7a9d93ac2d7dd5f52005a5a1bf9
Winsor <winsoromar12@gmail.com>
2026-04-02 00:25:37 -0400

    feat: add initial app component with routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AppShellComponent } from './core/layout/app-shell';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppShellComponent,
+    children: [
+      {
+        path: 'gestion-de-puntos',
+        loadComponent: () =>
+          import('./points-management/points-management/points-management').then(
+            (m) => m.PointsManagement,
+          ),
+      },
+      { path: '', redirectTo: 'gestion-de-puntos', pathMatch: 'full' },
+    ],
+  },
+];
