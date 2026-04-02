4717467c890f33fc4a8ff27297316c32c44a74d2
Winsor <winsoromar12@gmail.com>
2026-04-02 00:27:28 -0400

    feat: add app shell

---
+++ src/app/core/layout/app-shell.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TopAppBarComponent } from './top-app-bar';
+import { BottomNavBarComponent } from './bottom-nav-bar';
+
+@Component({
+  selector: 'app-shell',
+  imports: [RouterOutlet, TopAppBarComponent, BottomNavBarComponent],
+  template: `
+    <app-top-app-bar title="Gestión de Puntos" icon="capture"></app-top-app-bar>
+    <router-outlet></router-outlet>
+    <app-bottom-nav-bar></app-bottom-nav-bar>
+  `,
+})
+export class AppShellComponent {}
